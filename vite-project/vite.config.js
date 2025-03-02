d2b06a835993b7908c3899cd2a6adfce2ab2dacf
lucifer_112 <Krishnafauj02@gmail.com>
2025-03-03 02:48:14 +0530

    build issue solved

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Specify the output directory as 'build'
+  },
   plugins: [react(),tailwindcss(),],
 })
